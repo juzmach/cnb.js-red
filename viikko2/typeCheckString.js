58317c48968d1ed889bb6cd6123b2b59b3468aa0
Tomi Virtanen <cobrelli@gmail.com>
2013-11-12 18:49:58 +0200

    string check

---
+++ viikko2/typeCheckString.js
@@ -0,0 +1,8 @@
+// Checking the type of given variable
+function typeCheckString(variable){
+	// Checks if the variable is number
+	if(typeof variable === "string"){
+		return true;
+	}
+	return false;
+};
