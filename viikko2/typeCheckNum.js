6f25c1f5e6f4c4c40bfb4a94a34c594256db5f5b
Tomi Virtanen <cobrelli@gmail.com>
2013-11-12 17:46:46 +0200

    num check

---
+++ viikko2/typeCheckNum.js
@@ -0,0 +1,8 @@
+// Checking the type of given variable
+function typeCheckNum(variable){
+	// Checks if the variable is number
+	if(typeof variable === "number"){
+		return true;
+	}
+	return false;
+};
